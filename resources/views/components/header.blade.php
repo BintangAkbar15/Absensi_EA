0e3fbd83184a7dd68e76b808b1d68e4a3db56510
Shilycia <bintangputraadryan103@gmail.com>
2024-09-24 16:44:55 +0700

    penambahan tampilan page master(menu)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class="col-12 w-100 d-flex align-items-center text-light" style="height: 60px; background: #7d2fd0;">
+<header class="col-12 w-100 d-flex align-items-center text-light sticky-top" style="height: 60px; background: #6f42c1;">
     <i class="fa-solid fa-arrow-left fs-4 fw-bold ms-5"></i>
     <label for="Title" class="h5 mt-2 ms-3">Absensi Excellent Academy</label>
 </header>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,18 +10,58 @@
 </head>
 <body>
     <x-header></x-header>
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="..." alt="First slide">
+            <img src="https://www.hidros.com.tr/wp-content/uploads/2023/01/slider-dump-trailer.png" class="d-block w-100" alt="First slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Second slide">
+            <img src="https://www.hidros.com.tr/wp-content/uploads/2023/01/slider-dump-trailer.png" class="d-block w-100" alt="Second slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Third slide">
+            <img src="https://www.hidros.com.tr/wp-content/uploads/2023/01/slider-dump-trailer.png" class="d-block w-100" alt="Third slide">
           </div>
         </div>
-      </div>
+    </div>
+    <div class="mt-2 mt-md-5 d-md-block d-none">
+        <div class="w-100 d-flex justify-content-center gap-sm-3 gap-1 flex-column flex-sm-row">
+            <div class="shadow border-primary border py-1 py-md-3 d-flex align-items-center justify-content-center col-1 col-sm-1 col-md-2 px-5" style="cursor: pointer;">
+                <label for="" class="fs-6 fs-sm-5 fs-md-4 fs-lg-3 me-2" style="cursor: pointer;">Siswa</label> <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="shadow border-primary border py-1 py-md-3 d-flex align-items-center justify-content-center col-1 col-sm-1 col-md-2 px-5" style="cursor: pointer;">
+                <label for="" class="fs-6 fs-sm-5 fs-md-4 fs-lg-3 me-2" style="cursor: pointer;">Kelas</label> <i class="fa-solid fa-chalkboard"></i>
+            </div>
+            <div class="shadow border-primary border py-1 py-md-3 d-flex align-items-center justify-content-center col-1 col-sm-1 col-md-2 px-5" style="cursor: pointer;">
+                <label for="" class="fs-6 fs-sm-5 fs-md-4 fs-lg-3 me-2" style="cursor: pointer;">Absensi</label> <i class="fa-solid fa-book"></i>
+            </div>
+            <div class="shadow border-primary border py-1 py-md-3 d-flex align-items-center justify-content-center col-1 col-sm-1 col-md-2 px-5" style="cursor: pointer;">
+                <label for="" class="fs-6 fs-sm-5 fs-md-4 fs-lg-3 me-2" style="cursor: pointer;">Laporan</label> <i class="fa-solid fa-chart-simple"></i>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="col-12 d-flex justify-content-around mt-5 d-md-none">
+            <div class="col-4 text-light d-flex flex-column align-items-center justify-content-center rounded shadow" style="height: 120px; background: #6f42c1">
+                <i class="fa-solid fa-user"></i>
+                <label for="">Siswa</label>
+            </div>
+            <div class="col-4 text-light d-flex flex-column align-items-center justify-content-center rounded shadow" style="height: 120px; background: #6f42c1">
+                <i class="fa-solid fa-chalkboard"></i>
+                <label for="">Kelas</label>
+            </div>    
+        </div>
+        
+        <div class="col-12 d-flex justify-content-around mt-5 d-md-none">
+            <div class="col-4 text-light d-flex flex-column align-items-center justify-content-center rounded shadow" style="height: 120px; background: #6f42c1">
+                <i class="fa-solid fa-book"></i>
+                <label for="">Absensi</label>
+            </div>
+            <div class="col-4 text-light d-flex flex-column align-items-center justify-content-center rounded shadow" style="height: 120px; background: #6f42c1">
+                <i class="fa-solid fa-chart-simple"></i>
+                <label for="">Laporan</label>
+            </div>    
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>      
 </body>
 </html>
