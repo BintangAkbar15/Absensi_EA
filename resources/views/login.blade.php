94bb2445460de5e6e90dd7122f945dfc0472f7cc
Shilycia <bintangputraadryan103@gmail.com>
2024-09-24 13:58:04 +0700

    penambahan page login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="d-flex align-items-center vh-100 justify-content-center flex-column">
+    <section class="p-3 p-md-4 p-xl-5 col-11 shadow border-2 border-dark">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6 bsb-tpl-bg-platinum">
+              <div class="d-flex flex-column justify-content-start h-100 p-3  p-xl-5">
+                <h3 class="m-0">Welcome!</h3>
+                <img class="img-fluid rounded mx-auto mt-5" loading="lazy" src="https://img.freepik.com/premium-vector/abstract-logo-design-any-corporate-business-company_1253202-92187.jpg" width="300" alt="BootstrapBrain Logo">
+              </div>
+            </div>
+            <div class="col-12 col-md-6 bsb-tpl-bg-lotion">
+              <div class="p-3 p-md-4 p-xl-5">
+                <div class="row">
+                  <div class="col-12">
+                    <div class="mb-5">
+                      <h3>Log in</h3>
+                    </div>
+                  </div>
+                </div>
+                <form action="#!">
+                  <div class="row gy-3 gy-md-4 overflow-hidden">
+                    <div class="col-12">
+                      <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
+                      <input type="text" class="form-control" name="username" id="username" required>
+                    </div>
+                    <div class="col-12">
+                      <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
+                      <input type="password" class="form-control" name="password" id="password" value="" required>
+                    </div>
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" name="remember_me" id="remember_me">
+                        <label class="form-check-label text-secondary" for="remember_me">
+                          Keep me logged in
+                        </label>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <div class="d-grid">
+                        <button class="btn bsb-btn-xl btn-primary" type="submit">Log in now</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
