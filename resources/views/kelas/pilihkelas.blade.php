55f9544b7272459307b86c18b2163faf046967e2
BintangAkbar15 <bintangakbar154@gmail.com>
2024-09-26 13:11:16 +0700

    menambahkan bangku tersisa

--- resources/views/kelas/pilihkelas.blade.php
+++ resources/views/kelas/pilihkelas.blade.php
@@ -3,11 +3,13 @@
             <div class="w-100 d-flex justify-content-center">
                 <label for="" class="h2 fw-bold mt-5">Pilih Kelas</label>
             </div>
+            {{-- {{ dd($data) }} --}}
             @forelse ($data as $kls)
                 <div class="d-flex flex-wrap flex-column flex-xl-row gap-3 gap-xl-5 col-12 mt-5 align-items-center justify-content-center">
                     <button class="col-11 justify-content-start ps-4 ps-xl-0 h1 align-items-center col-xl-2 shadow text-xl-center text-right py-xl-5 rounded bg-warning d-flex flex-xl-column justify-xl-content-center align-items-center" style="height: fit-content">
                         <i class="fa-solid col-2 col-xl-12 fa-chalkboard mb-xl-3 py-3" ></i>
                         <label for="" class="h5 col-10 text-end pe-4 pe-xl-0 text-xl-center col-xl-12">{{ $kls->name }}</label>
+                        <label for="" class="h5 col-10 text-end pe-4 pe-xl-0 text-xl-center col-xl-12">Bangku Tersisa : {{ abs($kls->bangku_tersisa - $kls->jumlah_siswa) }}</label>
                     </button>
                 </div>
             @empty
