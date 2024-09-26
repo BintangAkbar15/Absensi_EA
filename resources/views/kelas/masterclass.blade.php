d75c8f97904b68c5d8789b6eae197dcb39983ee7
BintangAkbar15 <bintangakbar154@gmail.com>
2024-09-26 13:02:45 +0700

    add kelas in pilihkelas.php

--- resources/views/kelas/masterclass.blade.php
+++ resources/views/kelas/masterclass.blade.php
@@ -4,7 +4,7 @@
             <label for="" class="h2 fw-bold mt-5">MASTER CLASS</label>
         </div>
         <div class="d-flex flex-wrap flex-column flex-xl-row gap-3 gap-xl-5 col-12 mt-5 align-items-center justify-content-center">
-            <a href="{{ route('siswa.kelas') }}" class="col-11 text-decoration-none justify-content-start ps-4 ps-xl-0 h1 align-items-center col-xl-2 shadow text-xl-center text-right py-xl-5 rounded bg-warning d-flex flex-xl-column justify-xl-content-center align-items-center" style="height: fit-content">
+            <a href="{{ route('siswa.kelas.pilih') }}" class="col-11 text-decoration-none justify-content-start ps-4 ps-xl-0 h1 align-items-center col-xl-2 shadow text-xl-center text-right py-xl-5 rounded bg-warning d-flex flex-xl-column justify-xl-content-center align-items-center" style="height: fit-content">
                 <i class="fa-solid col-2 col-xl-12 fa-user-plus mb-xl-3 py-3" ></i>
                 <label for="" class="h5 col-10 text-end pe-4 pe-xl-0 text-xl-center col-xl-12">Tambah Siswa ke kelas</label>
             </a>
