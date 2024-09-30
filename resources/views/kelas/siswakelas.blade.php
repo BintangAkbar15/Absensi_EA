5cf2d756a57c773f881f32bf2d1ab8d2fbddd006
Shilycia <bintangputraadryan103@gmail.com>
2024-09-30 13:59:33 +0700

    perbaikan bug

--- resources/views/kelas/siswakelas.blade.php
+++ resources/views/kelas/siswakelas.blade.php
@@ -93,8 +93,24 @@
                 
                 // Jika checkbox ditemukan, ubah status checked
                 if (checkbox) {
-                    checkbox.checked = !checkbox.checked; // Toggle status checkbox (checked/unchecked)
+                    checkbox.checked = !checkbox.checked; 
+                    let checkboxes = document.querySelectorAll('input[type=checkbox]');
+                    let addallButtons = document.querySelectorAll('.add_all'); 
+
+                    checkboxes.forEach(function(checkbox) {
+                        let isAnyChecked = Array.from(checkboxes).some(cb => cb.checked);
+
+                            
+                        addallButtons.forEach(function(button) {
+                            if (isAnyChecked) {
+                                button.style.display = 'block'; 
+                            } else {
+                                button.style.display = 'none'; 
+                            }
+                        });
+                    });
                 }
+
             });
         });
     </script>
